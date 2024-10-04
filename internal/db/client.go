5dc3dcf3b6f37c83b469e9740da73afbd84abd0d
Rakhulsr <shomarizky123@gmail.com>
2024-10-04 16:05:30 +0700

    updates file

--- internal/db/client.go
+++ internal/db/client.go
@@ -3,9 +3,11 @@ package db
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -15,12 +17,15 @@ type RedisClient struct {
 
 func NewRedisClient() *RedisClient {
 
+	godotenv.Load()
+
+	residURL := os.Getenv("REDIS_URL")
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 
 	defer cancel()
 
 	client := redis.NewClient(&redis.Options{
-		Addr:     "redis:6379",
+		Addr:     residURL,
 		Password: "",
 		DB:       0,
 	})
